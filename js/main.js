cdcc3ba4198deed519781f59ee1b31f9e680316d
denis-vishnyakov <denis@vishnyakov.dev>
2023-11-09 12:55:59 +0300

    beta cart

--- js/main.js
+++ js/main.js
@@ -7,9 +7,12 @@ window.addEventListener('DOMContentLoaded', () => {
     
     document.querySelector('.header__flex_cart').addEventListener('click', () => {
         modalCart.classList.add('active');
+        document.body.style = 'overflow: hidden';
     })
 
     document.querySelector('#cart-close').addEventListener('click', () => {
         modalCart.classList.remove('active');
+        document.body.style = 'overflow: inherit';
     })
+
 })
